f495dc9cedf2f97f4505eb11a621a7d1e8de7698
Samer Iliya <sameriliya@gmail.com>
2022-12-14 09:10:32 +0000

    adding some updates to the webpage, including emojis!

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ st.set_page_config(
             initial_sidebar_state="auto") # collapsed
 
 
-'''
-# DelAI
-'''
+st.markdown('''
+# :airplane: DelAI :airplane:
+''')
 
 st.markdown('''
-Please enter your flight details...
+Please enter your flight details... :
 ''')
 
 col1, col2 = st.columns(2)
@@ -38,29 +38,34 @@ if st.button('Search for flight!'):
     with st.spinner("Locating your flight details..."):
         X_new = get_processed_flight_details(flight_number, d)
         if type(X_new) != pd.DataFrame:
-            f"Couldn't find a flight with those details, please try again!"
+            st.markdown(f"Couldn't find a flight with those details, please try again!")
         else:
-            origin = get_airport_details_dict(X_new['Origin'][0])
-            dest = get_airport_details_dict(X_new['Dest'][0])
-            dep_time = datetime.datetime.strptime(str(X_new['CRSDepTime'][0]).zfill(4),'%H%M').strftime('%H:%M')
-            arr_time = datetime.datetime.strptime(str(X_new['CRSArrTime'][0]).zfill(4),'%H%M').strftime('%H:%M')
-            f'''
-            ## Your flight:
-            ### **{origin['name']}** to **{dest['name']}**
-            ### Sched. Dep Time: {dep_time}
-            ### Sched. Arr Time: {arr_time}
-            '''
+            with st.expander("See your flight details"):
+                origin = get_airport_details_dict(X_new['Origin'][0])
+                dest = get_airport_details_dict(X_new['Dest'][0])
+                dep_time = datetime.datetime.strptime(str(X_new['CRSDepTime'][0]).zfill(4),'%H%M').strftime('%H:%M')
+                arr_time = datetime.datetime.strptime(str(X_new['CRSArrTime'][0]).zfill(4),'%H%M').strftime('%H:%M')
+                st.markdown(f'''
+                        ## Your flight:
+                        ### {origin['name']} to {dest['name']}
+                        ### 🛫 Sched. Dep Time: {dep_time}
+                        ### 🛬 Sched. Arr Time: {arr_time}
+                            ''')
 if st.button('Predict my delay!'):
     # with st.spinner("Working hard to get your prediction..."):
-    prediction = pred(flight_number=flight_number, date=d)
+    prediction = round(pred(flight_number=flight_number, date=d)*100,2)
     st.success("Successfully predicted your chance of delay!")
     st.balloons()
 
-    f'''
+    st.markdown(f'''
     # Our prediction:
-    ## {round(prediction*100, 2)}% chance of flight delay of 30 mins or more!
-    '''
-
+    ## {round(prediction)}% chance of delay in your arrival of 30 mins or more!
+    ''')
+    with st.expander('Disclaimer'):
+        if prediction <= 15:
+            st.markdown("It's looking good! Your flight is probably going to be on time :relaxed: but it's always best to check with your airline!")
+        else:
+            st.markdown("Don't be silly and have an extra beer at the airport. Check the departure board")
 # df = pd.DataFrame(
 #     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
 #     columns=['lat', 'lon'])
